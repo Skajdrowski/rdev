32c735521e78e32a641427ded37c4c96a9e01e0f
Skajdrovski <cssteam1337pl@gmail.com>
2025-04-15 18:36:50 +0200

    Update dependencies

--- src/windows/listen.rs
+++ src/windows/listen.rs
@@ -1,3 +1,5 @@
+#![allow(static_mut_refs)]
+
 use crate::rdev::{Event, EventType, ListenError};
 use crate::windows::common::{convert, set_key_hook, set_mouse_hook, HookError, HOOK, KEYBOARD};
 use std::os::raw::c_int;
